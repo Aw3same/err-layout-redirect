91d1aee25ecae0ae2898569330f083db80665861
Aw3same <Aw3same@users.noreply.github.com>
2023-05-31 09:04:59 +0200

    2 files modified

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { WrappedApp } from './App';
+import { AuthProvider } from './context/AuthContext';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <WrappedApp />
+    <AuthProvider>
+				<WrappedApp />
+		</AuthProvider>
   </StrictMode>
 );
